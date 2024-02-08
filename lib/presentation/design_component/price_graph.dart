3aec0ee43ca875dd36eb73a2015b8e6f908188aa
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-02-08 16:09:36 +0900

    host

--- lib/presentation/design_component/price_graph.dart
+++ lib/presentation/design_component/price_graph.dart
@@ -25,50 +25,47 @@ class _PriceGraphState extends State<PriceGraph> {
 
     double widthBetweenPoint = MediaQuery.of(context).size.width / (widget.priceList.length);
 
-    return SizedBox(
-      width: 350,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Sparkline(
-            useCubicSmoothing: true,
-            data: widget.priceList,
-            lineColor: Colors.indigoAccent,
-            min: 0,
-            lineWidth: 2.0,
-            pointsMode: PointsMode.none,
-            maxLabel: false,
-            pointSize: 5.0,
-            averageLine: true,
-            maxLine: true,
-            averageLabel: false,
-            pointColor: Colors.indigo,
-            fillMode: FillMode.below,
-            fillGradient: const LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.indigoAccent,
-                Colors.white,
-              ],
-            ),
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Sparkline(
+          useCubicSmoothing: true,
+          data: widget.priceList,
+          lineColor: Colors.indigoAccent,
+          min: 0,
+          lineWidth: 2.0,
+          pointsMode: PointsMode.none,
+          maxLabel: false,
+          pointSize: 5.0,
+          averageLine: true,
+          maxLine: true,
+          averageLabel: false,
+          pointColor: Colors.indigo,
+          fillMode: FillMode.below,
+          fillGradient: const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.indigoAccent,
+              Colors.white,
+            ],
           ),
-          // Positioned(
-          //   bottom: 100,
-          //   left: widthBetweenPoint * (highestPriceIndex) - (highestPriceIndex == widget.priceList.length - 1 ? 20 : 0),
-          //   child: Text(
-          //     "최고 ${widget.priceList[highestPriceIndex].toInt().toCommaFormat}원",
-          //   ),
-          // ),
-          // Positioned(
-          //   bottom: -20,
-          //   left: (widthBetweenPoint) * (lowestPriceIndex) - (lowestPriceIndex == widget.priceList.length - 1 ? 20 : 0),
-          //   child: Text(
-          //     "최저 ${widget.priceList[lowestPriceIndex].toInt().toCommaFormat}원",
-          //   ),
-          // )
-        ],
-      ),
+        ),
+        // Positioned(
+        //   bottom: 100,
+        //   left: widthBetweenPoint * (highestPriceIndex) - (highestPriceIndex == widget.priceList.length - 1 ? 20 : 0),
+        //   child: Text(
+        //     "최고 ${widget.priceList[highestPriceIndex].toInt().toCommaFormat}원",
+        //   ),
+        // ),
+        // Positioned(
+        //   bottom: -20,
+        //   left: (widthBetweenPoint) * (lowestPriceIndex) - (lowestPriceIndex == widget.priceList.length - 1 ? 20 : 0),
+        //   child: Text(
+        //     "최저 ${widget.priceList[lowestPriceIndex].toInt().toCommaFormat}원",
+        //   ),
+        // )
+      ],
     );
   }
 }
