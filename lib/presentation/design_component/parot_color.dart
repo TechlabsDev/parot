73572230aba14285b77a2f3fad2c8f8da4206292
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-02-14 10:35:38 +0900

    색상 추가

---
+++ lib/presentation/design_component/parot_color.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ParotColor {
+  /*gray*/
+  static const Color gray900 = Color(0xFF121212);
+  static const Color gray800 = Color(0xFF222222);
+  static const Color gray700 = Color(0xFF424242);
+  static const Color gray600 = Color(0xFF5D5D5D);
+  static const Color gray500 = Color(0xFF888888);
+  static const Color gray400 = Color(0xFF999999);
+  static const Color gray300 = Color(0xFFB2B2B2);
+  static const Color gray200 = Color(0xFFC0C0C0);
+  static const Color gray100 = Color(0xFFDBDBDB);
+
+  /*red*/
+  static const Color red500 = Color(0xFFFF3519);
+
+  /*blue*/
+  static const Color blue500 = Color(0xFF2553F8);
+
+  /*green*/
+  static const Color green500 = Color(0xFF4BD984);
+
+  /*yellow*/
+  static const Color yellow500 = Color(0xFFFFCF52);
+}
