5ae29d813efbf1f96db8936ae64e9e687c846f2e
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-02-28 10:44:26 +0900

    계속해서 그래프 그리는 중

--- lib/presentation/design_component/parrot_price_graph.dart
+++ lib/presentation/design_component/parrot_price_graph.dart
@@ -23,130 +23,148 @@ class _ParrotPriceGraphState extends State<ParrotPriceGraph> {
     int highestPrice = widget.priceList.reduce((value, element) => value > element ? value : element);
     int lowestPrice = widget.priceList.reduce((value, element) => value < element ? value : element);
 
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: Column(
-            children: [
-              const Row(
-                children: [
-                  Text(
-                    "가격 변동 그래프",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Spacer(),
-                ],
+        const Row(
+          children: [
+            Text(
+              "가격 변동 그래프",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
               ),
-              const SizedBox(height: 20),
-              const Text(
-                "그래프 BAD...",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: ParrotColor.red500,
+            ),
+            Spacer(),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: ParrotColor.red50,
+            ),
+            width: double.maxFinite,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: const Column(
+              children: [
+                Text(
+                  "그래프 BAD...",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: ParrotColor.red500,
+                  ),
                 ),
-              ),
-              const Text(
-                "이해가 안 되네 대체 왜 그라디언트가 반대로 들어가는거임?",
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w700,
-                  color: ParrotColor.red500,
+                Text(
+                  "평균가보다 훨씬 비싸요! 지금 사면 손해예요",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    color: ParrotColor.red500,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Stack(
-                children: [
-                  ClipPath(
-                    clipper: ParrotChartClipper(
-                      color: ParrotColor.gray200,
-                      priceList: widget.priceList,
-                    ),
-                    child: Container(
-                      width: widget.graphSize == null ? 300 : widget.graphSize!.width,
-                      height: widget.graphSize == null ? 150 : widget.graphSize!.height,
-                      color: ParrotColor.red100,
-                    ),
-                  ),
-                  CustomPaint(
-                    size: widget.graphSize ?? const Size(300, 150),
-                    foregroundPainter: ParrotChartPainter(
-                      color: ParrotColor.red500,
-                      priceList: widget.priceList,
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 15,
-                    child: SizedBox(
-                      width: widget.graphSize == null ? 300 : widget.graphSize!.width,
-                      child: Row(
-                        children: [
-                          Text(
-                            widget.startDate.MMDD,
-                            style: const TextStyle(
-                              color: ParrotColor.gray400,
-                              fontSize: 11,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          const Spacer(),
-                          const Text(
-                            "오늘",
-                            style: TextStyle(
-                              color: ParrotColor.gray400,
-                              fontSize: 11,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-        Column(
+        const SizedBox(height: 20),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 90),
-            Text(
-              highestPrice.toCommaFormat,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 11,
-                color: ParrotColor.red500,
+            Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Stack(
+                    children: [
+                      ClipPath(
+                        clipper: ParrotChartClipper(
+                          color: ParrotColor.gray200,
+                          priceList: widget.priceList,
+                        ),
+                        child: Container(
+                          width: widget.graphSize == null ? 300 : widget.graphSize!.width,
+                          height: widget.graphSize == null ? 150 : widget.graphSize!.height,
+                          color: ParrotColor.red50,
+                        ),
+                      ),
+                      CustomPaint(
+                        size: widget.graphSize ?? const Size(300, 150),
+                        foregroundPainter: ParrotChartPainter(
+                          color: ParrotColor.red500,
+                          priceList: widget.priceList,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 15,
+                        child: SizedBox(
+                          width: widget.graphSize == null ? 300 : widget.graphSize!.width,
+                          child: Row(
+                            children: [
+                              Text(
+                                widget.startDate.MMDD,
+                                style: const TextStyle(
+                                  color: ParrotColor.gray400,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const Spacer(),
+                              const Text(
+                                "오늘",
+                                style: TextStyle(
+                                  color: ParrotColor.gray400,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 40),
-            Text(
-              averagePrice.toCommaFormat,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 11,
-                color: ParrotColor.gray800,
-              ),
-            ),
-            const SizedBox(height: 40),
-            Text(
-              lowestPrice.toCommaFormat,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 11,
-                color: ParrotColor.blue500,
-              ),
+            Column(
+              children: [
+                const SizedBox(height: 10),
+                Text(
+                  highestPrice.toCommaFormat,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 11,
+                    color: ParrotColor.red500,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Text(
+                  averagePrice.toCommaFormat,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 11,
+                    color: ParrotColor.gray800,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Text(
+                  lowestPrice.toCommaFormat,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 11,
+                    color: ParrotColor.blue500,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
