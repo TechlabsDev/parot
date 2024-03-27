b8a7bf036c4f0ada3a17a3c2c80081b7a51b268f
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-03-27 14:21:13 +0900

    약관 동의 목록 구현

--- lib/presentation/sign_up/widget/sign_up_term_agree_list.dart
+++ lib/presentation/sign_up/widget/sign_up_term_agree_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parot/presentation/sign_up/controller/sign_up_controller.dart';
 
 import '../../design_component/parrot_check_circle_in_row.dart';
 import '../../design_component/parrot_color.dart';
@@ -13,16 +15,22 @@ class SignUpTermAgreeList extends StatefulWidget {
 class _SignUpTermAgreeListState extends State<SignUpTermAgreeList> {
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<SignUpController>();
     Map<int, String> indexToTextMap = {
       0: "(필수) 이용약관 동의",
       1: "(필수) 개인정보 수집 및 이용 동의",
       2: "(선택) 개인정보 마케팅 활용 동의",
     };
+    Map<int, RxBool> indexToRxValue = {
+      0: controller.termAgree,
+      1: controller.privacyCollectAndUseAgree,
+      2: controller.privacyMarketingUseAgree,
+    };
     //항목을 동의/비동의 했을 때
     Map<int, Function(bool)> indexToFunctionMap = {
-      0: (v) {},
-      1: (v) {},
-      2: (v) {},
+      0: controller.setTermAgree,
+      1: controller.setPrivacyCollectAndUseAgree,
+      2: controller.setPrivacyMarketingUseAgree,
     };
     //[보기] 버튼 눌렀을 때
     Map<int, Function> indexToTrailFunctionMap = {
@@ -30,51 +38,55 @@ class _SignUpTermAgreeListState extends State<SignUpTermAgreeList> {
       1: () {},
       2: () {},
     };
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: ParrotCheckCircleInRow(
-            value: false,
-            onChanged: (_) {},
-            titleText: const Text(
-              "전체 약관 동의",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-        ),
-        const Divider(color: ParrotColor.gray50, height: 40),
-        for (int i = 0; i < 3; i++)
-          Padding(
-            padding: EdgeInsets.only(left: 16, bottom: i == 2 ? 0 : 16, top: i == 0 ? 0 : 16, right: 16),
-            child: ParrotCheckCircleInRow(
-              value: false,
-              onChanged: indexToFunctionMap[i]!,
-              titleText: Text(
-                indexToTextMap[i]!,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              trailing: InkWell(
-                onTap: () => indexToTrailFunctionMap[i]!,
-                child: const Text(
-                  "보기",
-                  style: TextStyle(
-                    fontSize: 11,
-                    fontWeight: FontWeight.w500,
-                    color: ParrotColor.gray300,
+    return GetX<SignUpController>(
+        init: Get.put(SignUpController()),
+        builder: (controller) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: ParrotCheckCircleInRow(
+                  value: controller.allTermAgree.value,
+                  onChanged: controller.setAllTermAgree,
+                  titleText: const Text(
+                    "전체 약관 동의",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-      ],
-    );
+              const Divider(color: ParrotColor.gray50, height: 40),
+              for (int i = 0; i < 3; i++)
+                Padding(
+                  padding: EdgeInsets.only(left: 16, bottom: i == 2 ? 0 : 16, top: i == 0 ? 0 : 16, right: 16),
+                  child: ParrotCheckCircleInRow(
+                    value: indexToRxValue[i]!.value,
+                    onChanged: indexToFunctionMap[i]!,
+                    titleText: Text(
+                      indexToTextMap[i]!,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    trailing: InkWell(
+                      onTap: () => indexToTrailFunctionMap[i]!,
+                      child: const Text(
+                        "보기",
+                        style: TextStyle(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w500,
+                          color: ParrotColor.gray300,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          );
+        });
   }
 }
