007ca9317e24b64d6743426fd7025d2827db3687
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-03-27 14:20:59 +0900

    회원 가입 환영 문구를 GetBuilder로 포장

--- lib/presentation/sign_up/widget/sign_up_welcome_text.dart
+++ lib/presentation/sign_up/widget/sign_up_welcome_text.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parot/presentation/sign_up/controller/sign_up_controller.dart';
 import 'package:styled_text/tags/styled_text_tag.dart';
 import 'package:styled_text/widgets/styled_text.dart';
 
@@ -14,36 +16,40 @@ class SignUpWelcomeText extends StatefulWidget {
 class _SignUpWelcomeTextState extends State<SignUpWelcomeText> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        StyledText(
-          text: "안녕하세요, 패럿<r>패럿</r>입니다",
-          tags: {
-            "r": StyledTextTag(
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-                color: ParrotColor.red500,
+    return GetBuilder<SignUpController>(
+        init: Get.put(SignUpController()),
+        builder: (controller) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              StyledText(
+                text: "안녕하세요, 패럿<r>패럿</r>입니다",
+                tags: {
+                  "r": StyledTextTag(
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: ParrotColor.red500,
+                    ),
+                  ),
+                },
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: ParrotColor.gray800,
+                ),
               ),
-            ),
-          },
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            color: ParrotColor.gray800,
-          ),
-        ),
-        const SizedBox(height: 4),
-        const Text(
-          "좀 더 정확한 가격 정보 제공을 위해\n약관 동의가 필요합니다.",
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-            color: ParrotColor.gray400,
-          ),
-        ),
-      ],
-    );
+              const SizedBox(height: 4),
+              const Text(
+                "좀 더 정확한 가격 정보 제공을 위해\n약관 동의가 필요합니다.",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: ParrotColor.gray400,
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
