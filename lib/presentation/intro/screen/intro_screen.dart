a9a7e4c602536847eaff0ec4c0a801ea08124d67
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-03-27 11:06:39 +0900

    인트로 화면 구현 중

--- lib/presentation/intro/screen/intro_screen.dart
+++ lib/presentation/intro/screen/intro_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:parot/presentation/design_component/parrot_color.dart';
 import 'package:parot/presentation/design_component/parrot_scaffold.dart';
 
 class IntroScreen extends StatefulWidget {
@@ -13,11 +14,35 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return ParrotScaffold(
-      horizontal16Padding: true,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
             height: 108.h,
+          ),
+          Image.asset("asset/logo/logo2.png", width: 201, height: 40),
+          const SizedBox(height: 16),
+          const Text(
+            "최저가 정보 받고\n현명한 소비를 시작하세요!",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: ParrotColor.gray400,
+            ),
+          ),
+          const Spacer(),
+          const Center(
+            child: Text(
+              "간편하게 시작하기",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: ParrotColor.gray200,
+              ),
+            ),
+          ),
+          Row(
+            children: [],
           ),
         ],
       ),
