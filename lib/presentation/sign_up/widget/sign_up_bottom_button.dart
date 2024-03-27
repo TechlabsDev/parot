f136ce38e6788d30fa86dd7388f2ee929a1ca23f
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-03-27 16:31:12 +0900

    회원가입 단계 버튼 분리

---
+++ lib/presentation/sign_up/widget/sign_up_bottom_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parot/presentation/sign_up/controller/sign_up_controller.dart';
+
+import '../../../const/enum/sign_up_step.dart';
+import '../../design_component/parrot_color.dart';
+import '../../design_component/parrot_elavated_button.dart';
+
+class SignUpBottomButton extends StatefulWidget {
+  const SignUpBottomButton({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpBottomButton> createState() => _SignUpBottomButtonState();
+}
+
+class _SignUpBottomButtonState extends State<SignUpBottomButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GetX<SignUpController>(builder: (controller) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ParrotElevatedButton(
+          enabled: controller.requiredTermAgree.isTrue,
+          onPressed: () {
+            int nextIndex = controller.pageController.page!.toInt() + 1;
+            controller.pageController.animateToPage(nextIndex, duration: const Duration(milliseconds: 150), curve: Curves.easeIn);
+          },
+          backgroundColor: ParrotColor.red500,
+          borderColor: ParrotColor.red500,
+          text: controller.step.value == SignUpStep.term
+              ? "다음"
+              : controller.step.value == SignUpStep.selectCategory
+                  ? "회원가입"
+                  : controller.step.value == SignUpStep.finish
+                      ? "시작하기"
+                      : "",
+          size: const Size(double.maxFinite, 52),
+        ),
+      );
+    });
+  }
+}
--- lib/presentation/sign_up/widget/sign_up_term_agree_page.dart
+++ lib/presentation/sign_up/widget/sign_up_term_agree_page.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:parot/const/enum/sign_up_step.dart';
 import 'package:parot/presentation/sign_up/controller/sign_up_controller.dart';
 import 'package:parot/presentation/sign_up/widget/sign_up_term_agree_list.dart';
 import 'package:parot/presentation/sign_up/widget/sign_up_welcome_text.dart';
-
-import '../../design_component/parrot_color.dart';
-import '../../design_component/parrot_elavated_button.dart';
 
 class SignUpTermAgreePage extends StatefulWidget {
   const SignUpTermAgreePage({Key? key}) : super(key: key);
@@ -19,7 +15,7 @@ class SignUpTermAgreePage extends StatefulWidget {
 class _SignUpTermAgreePageState extends State<SignUpTermAgreePage> {
   @override
   Widget build(BuildContext context) {
-    return GetX<SignUpController>(builder: (controller) {
+    return GetBuilder<SignUpController>(builder: (controller) {
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -35,21 +31,6 @@ class _SignUpTermAgreePageState extends State<SignUpTermAgreePage> {
           ),
           const Spacer(),
           const SignUpTermAgreeList(),
-          SizedBox(height: 32.h),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: ParrotElevatedButton(
-              enabled: controller.requiredTermAgree.isTrue,
-              onPressed: () {
-                controller.step.value = SignUpStep.finish;
-                controller.pageController.animateToPage(1, duration: const Duration(milliseconds: 150), curve: Curves.easeIn);
-              },
-              backgroundColor: ParrotColor.red500,
-              borderColor: ParrotColor.red500,
-              text: "다음",
-              size: const Size(double.maxFinite, 52),
-            ),
-          ),
         ],
       );
     });
