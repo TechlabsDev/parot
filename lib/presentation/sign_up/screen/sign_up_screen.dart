e9c609c3f7413feedd65477cc5cfd9f1a8b3812b
goehd2538@amanda.co.kr <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2024-03-27 14:05:12 +0900

    회원가입 화면 구현 중

--- lib/presentation/sign_up/screen/sign_up_screen.dart
+++ lib/presentation/sign_up/screen/sign_up_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:parot/presentation/design_component/parrot_color.dart';
 import 'package:parot/presentation/design_component/parrot_depth_header.dart';
 import 'package:parot/presentation/design_component/parrot_scaffold.dart';
 import 'package:parot/presentation/sign_up/controller/sign_up_controller.dart';
-import 'package:styled_text/styled_text.dart';
+import 'package:parot/presentation/sign_up/widget/sign_up_term_agree_list.dart';
+import 'package:parot/presentation/sign_up/widget/sign_up_welcome_text.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -21,6 +21,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
       init: Get.put(SignUpController()),
       builder: (controller) {
         return ParrotScaffold(
+          horizontal16Padding: false,
           appBar: ParrotDepthHeader(
             title: "회원가입",
             actions: [],
@@ -30,34 +31,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 32.h),
-              Image.asset("asset/logo/logo2.png", width: 144),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Image.asset("asset/logo/logo2.png", width: 144),
+              ),
               SizedBox(height: 32.h),
-              StyledText(
-                text: "안녕하세요, 패럿<r>패럿</r>입니다",
-                tags: {
-                  "r": StyledTextTag(
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: ParrotColor.red500,
-                    ),
-                  ),
-                },
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: ParrotColor.gray800,
-                ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: SignUpWelcomeText(),
               ),
-              const SizedBox(height: 4),
-              const Text(
-                "좀 더 정확한 가격 정보 제공을 위해\n약관 동의가 필요합니다.",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: ParrotColor.gray400,
-                ),
-              ),
+              const Spacer(),
+              const SignUpTermAgreeList(),
             ],
           ),
         );
